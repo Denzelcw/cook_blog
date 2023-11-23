874e0ce9e31927fc4f6ce398efc61961c1c43f7d
Denis Gornov <you@example.com>
2023-11-24 02:06:44 +0300

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+
+from . import models
+
+
+class RecipeLine(admin.StackedInline):
+    model = models.Recipe
+    extra = 1
+
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['author', 'title', 'category', 'created_at', 'id']
+    inlines = [RecipeLine]
+
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'prep_time', 'cook_time', 'post']
+
+
+admin.site.register(models.Category, MPTTModelAdmin)
+admin.site.register(models.Tag)
+# admin.site.register(models.Post, PostAdmin)
+# admin.site.register(models.Recipe)
+admin.site.register(models.Comment)
